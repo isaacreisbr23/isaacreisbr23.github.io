8df106ef0da3d628a9a5996899718310299bd2df
isaacreisbr23 <66951257+isaacreisbr23@users.noreply.github.com>
2022-01-18 17:55:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+function openDownloadBt(){
+    alert("Popups são carregados no topo do site")
+    var downPop = document.getElementById('DownloadPopup')
+    downPop.style = 'display:block;'
+}
+function closeDownloadBt(){
+    var downPop = document.getElementById('DownloadPopup')
+    downPop.style='display:none;'
+}
+
+function openConheca(){
+    alert("Popups são carregados no topo do site")
+    var conpop = document.getElementById('ConheçaPopup')
+    conpop.style = 'display:block;'
+}
+function closeConheca(){
+    var conpop = document.getElementById('ConheçaPopup')
+    conpop.style = 'display:none;'
+}
+
+function openAtts(){
+    alert("Popups são carregados no topo do site")
+    var attpop= document.getElementById('AttPopup')
+    attpop.style = 'display:block;'
+}
+function closeAtts(){
+    var attpop= document.getElementById('AttPopup')
+    attpop.style = 'display:none;'
+}
+
+function openReport(){
+    alert("Popups são carregados no topo do site")
+    var rprtpop= document.getElementById('ReportPopup')
+    rprtpop.style = 'display:block;'
+}
+function closeReport(){
+    var rprtpop= document.getElementById('ReportPopup')
+    rprtpop.style = 'display:none;'
+}
+
+function openInstallh(){
+    var instllpop= document.getElementById('InstallPopup')
+    instllpop.style = 'display:block;'
+}
+function closeInstallh(){
+    var instllpop= document.getElementById('InstallPopup')
+    instllpop.style = 'display:none;'
+}
